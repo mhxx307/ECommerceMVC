b3de4238228a4811e140ee152197769b1a638012
Minh Quan <69964747+mhxx307@users.noreply.github.com>
2024-11-28 11:30:24 +0700

    Config project
    create database, install necessary packages, connect to database, generate data (scafftafold-context)

--- ECommerceMVC_CSharp/Program.cs
+++ ECommerceMVC_CSharp/Program.cs
@@ -1,7 +1,14 @@
+using ECommerceMVC_CSharp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Hshop2023Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HShop"));
+});
 
 var app = builder.Build();
 
